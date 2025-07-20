cecf2ff3fa98853d81e73ea4d389be3dba099f5f
Christian Wimmer <wimmer@teamchris.info>
2025-07-20 17:12:13 +0200

    Fix Previous Hut Fix to prevent NPE.

--- src/Game.LoadSave.cs
+++ src/Game.LoadSave.cs
@@ -193,7 +193,10 @@ namespace CivOne
 
 				//CW: not sure why this happens to AI cities, 
 				// but a city tile may have a hut which reappears after a city is destroyed (funny).
-				city.Tile.Hut = false;
+				if (city.Tile != null)
+				{
+					city.Tile.Hut = false;
+				}
 
 				foreach (byte fortifiedUnit in cityData.FortifiedUnits)
                 {
