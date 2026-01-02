3c3c0229a61477908da1d53a06c2c9d1b0be46e5
Christian Wimmer <wimmer@teamchris.info>
2026-01-02 19:24:41 +0100

    Refactor: Full project is under cc0. There is no need to apply it for each file separately.

--- xunit/src/Mocks/MockPlayer.cs
+++ xunit/src/Mocks/MockPlayer.cs
@@ -1,14 +1,3 @@
-// CivOne
-//
-// To the extent possible under law, the person who associated CC0 with
-// CivOne has waived all copyright and related or neighboring rights
-// to CivOne.
-//
-// You should have received a copy of the CC0 legalcode along with this
-// work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
-
-// Author: Kevin Routley : July, 2019
-
 using System.Linq;
 using System.Collections.Generic;
 using CivOne.Wonders;
--- xunit/src/Mocks/MockedGrassland.cs
+++ xunit/src/Mocks/MockedGrassland.cs
@@ -1,14 +1,3 @@
-// CivOne
-//
-// To the extent possible under law, the person who associated CC0 with
-// CivOne has waived all copyright and related or neighboring rights
-// to CivOne.
-//
-// You should have received a copy of the CC0 legalcode along with this
-// work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
-
-// Author: Kevin Routley : July, 2019
-
 using CivOne.Tiles;
 using CivOne.Units;
 using System;
